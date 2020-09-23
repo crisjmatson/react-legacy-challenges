5df856cf7d202fc3d76a22094228ac7f766e4a12
Cris Matson <crisjmatson@gmail.com>
2020-09-23 14:55:53 -0400

    challenges complete

--- src/Components/ToDo/Task.js
+++ src/Components/ToDo/Task.js
@@ -1,55 +1,70 @@
 import React, { Component } from "react";
-import {
-	Form,
-	FormGroup,
-	Button,
-	Label,
-	ListGroup,
-	ListGroupItem,
-} from "reactstrap";
+import { Form, Button, Label, Input } from "reactstrap";
+import DisplayAll from "./DisplayAll";
 
 export default class Task extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			showTasks: true,
-			taskAdd: "unchanged",
+			showTasks: false,
+			taskAdd: "",
 			taskList: [],
+			render: false,
 		};
 	}
 
 	addTasks(event) {
 		event.preventDefault();
-		this.state.taskList.push({ name: this.state.taskAdd, complete: false }); //task added to end of arr
-		console.log(this.state.taskList);
+		this.state.taskList.push({ name: this.state.taskAdd, complete: false }); 
 	}
 
-	listDisplay() {
-		this.state.taskList.map((tsk) => {
-			return <li key={tsk}>{tsk}</li>;
-		});
-	}
+	/* 	updateList(list) {
+		let compare = this.state.taskList;
+		if (list === compare) {
+			return;
+		} else {
+			this.setState({ taskList: [list] });
+			console.log("list state updated!");
+			return;
+		}
+	} */
 
 	render() {
 		return (
-			<div>
+			<div onClick={() => this.setState({ render: !this.state.render })}>
 				<Form onSubmit={(event) => this.addTasks(event)}>
-					<Label>TASK ENTRY: </Label>
-					<br />
-					<input
+					<h2>
+						<Label>TASK ENTRY: </Label>
+					</h2>
+					<Input
 						type="text"
+						width=""
 						onChange={(event) => this.setState({ taskAdd: event.target.value })}
 						required
 					/>
 					<br />
-					<Button type="submit">submit</Button>
+					<Button className="challButton" type="submit">
+						submit
+					</Button>
 				</Form>
-				<Button onClick={() => this.setState({ showTasks: true })}>
-					view all tasks
-				</Button>
+				{this.state.showTasks ? (
+					<Button
+						className="challButton"
+						onClick={() => this.setState({ showTasks: !this.state.showTasks })}
+					>
+						hide tasks
+					</Button>
+				) : (
+					<Button
+						className="challButton"
+						onClick={() => this.setState({ showTasks: !this.state.showTasks })}
+					>
+						view tasks
+					</Button>
+				)}
 				{this.state.showTasks ? (
 					<ul>
-						<ListItems fullList={this.state.taskList} />
+						<DisplayAll fullList={this.state.taskList} />
 					</ul>
 				) : (
 					<span></span>
@@ -57,30 +72,4 @@ export default class Task extends Component {
 			</div>
 		);
 	}
-}
-
-function ListItems(props) {
-	function completeTask(taskName) {
-		console.log(taskName);
-	}
-	return (
-		<ListGroupItem>
-			{props.fullList.map((tsk) => {
-				return (
-					<ListGroupItem key={tsk.name}>
-						{tsk.name}
-						<br />
-						<label>complete?</label>
-						<br />
-						<input
-							type="checkbox"
-							name="completeTask"
-							onChange={props.onChange}
-							checked={tsk.complete}
-						/>
-					</ListGroupItem>
-				);
-			})}
-		</ListGroupItem>
-	);
 }
